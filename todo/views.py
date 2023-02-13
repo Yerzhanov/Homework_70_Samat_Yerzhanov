250b341b4e78ce1c1b9af66e5d7ae45c2ff42c19
Samat Yerzhanov <dearhauer@gmail.com>
2023-02-13 14:48:58 +0600

    Созданы функции отображния домашне

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,12 @@
 from django.db import IntegrityError
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
+
+
+def home(request):
+    return render (request, 'todo/home.html')
 
 
 def signupuser(request):
@@ -20,6 +24,23 @@ def signupuser(request):
                 return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Такой пользователь уже существует. Задайте новое имя.'})
         else:
             return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Пароль не совпадает'})
+
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Пользователь и пароль не найдены'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
 
 
 def currenttodos(request):
