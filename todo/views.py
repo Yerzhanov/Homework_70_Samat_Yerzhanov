9cc275e77e5d5987406fcb2fe5ae1d293decc7fb
Samat Yerzhanov <dearhauer@gmail.com>
2023-02-13 14:20:49 +0600

    Добавлены пути для регистрации и выхода пользователя и отражения страницы с делами

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 
 def signupuser(request):
@@ -8,8 +10,17 @@ def signupuser(request):
         return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'],password=request.POST['password'])
-            user.save()
+            try:
+                user = User.objects.create_user(request.POST['username'],password=request.POST['password'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Такой пользователь уже существует. Задайте новое имя.'})
         else:
-            print('Hello')
-            pass
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Пароль не совпадает'})
+
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html', {'form': UserCreationForm()})
