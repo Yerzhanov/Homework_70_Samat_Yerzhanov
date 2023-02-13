d71c4be16378061e475c660674557a6a14e393b5
Samat Yerzhanov <dearhauer@gmail.com>
2023-02-13 17:15:25 +0600

    Добавлен путь для завершения дела

--- To_do_list/urls.py
+++ To_do_list/urls.py
@@ -26,5 +26,9 @@ urlpatterns = [
 
     path('', views.home, name='home'),
     path('current/', views.currenttodos, name='currenttodos'),
-    path('create/', views.createtodo, name='createtodo')
+    path('create/', views.createtodo, name='createtodo'),
+
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+
 ]
