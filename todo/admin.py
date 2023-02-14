0a49d8e3a33bac39aad77544247461424e36f7d5
Samat Yerzhanov <dearhauer@gmail.com>
2023-02-14 17:15:22 +0600

    Добавил файлы проекта

---
+++ todo/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Todolist
+
+
+class TodolistAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_created',)
+
+
+admin.site.register(Todolist, TodolistAdmin)
+
+
