72c048a10e04ad9b70138c687cb09aedbefb1b50
Yerzhanov <dearhauer@gmail.com>
2023-03-03 17:20:38 +0600

    Созданы классовые контроллеры для просмотра домашней страницы и списка всех дел

--- todo/views.py
+++ todo/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
-from .forms import TodoForm
-from .models import Todolist
+from .forms import TodoForm, CommentForm
+from .models import Todolist, Comment
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 
-def home(request):
-    return render (request, 'todo/home.html')
+class HomeView(TemplateView):
+    template_name = 'todo/home.html'
+    extra_context = {'title': 'Домашняя страница'}
 
 
 def signupuser(request):
@@ -60,9 +63,34 @@ def createtodo(request):
             return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error': 'Были переданы неверные данные. Попробуйте снова.'})
 
 @login_required()
-def currenttodos(request):
-    todos = Todolist.objects.filter(user=request.user, date_completed__isnull=True)
-    return render(request, 'todo/currenttodos.html', {'todos': todos})
+def createcomment(request):
+    if request.method == 'GET':
+        return render(request, 'todo/create_comment.html', {'form': CommentForm()})
+    else:
+        try:
+            form = CommentForm(request.POST)
+            new_comment = form.save(commit=False)
+            new_comment.user = request.user
+            new_comment.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/create_comment.html', {'form': CommentForm(), 'error': 'Были переданы неверные данные. Попробуйте снова.'})
+
+@login_required()
+def viewcomment(request):
+    comments = Comment.objects.all()
+    return render(request, 'todo/view_comment.html', {'comments': comments})
+
+
+class CurrentView(ListView):
+    model = Todolist
+    template_name = 'todo/currenttodos.html'
+    extra_context = {'title': 'Общий список дел'}
+
+    def get_context_data(self, **kwargs):
+       context = super(CurrentView, self).get_context_data()
+       context['todos'] = Todolist.objects.all()
+       return context
 
 @login_required()
 def completedtodos(request):
@@ -97,5 +125,3 @@ def deletetodo(request, todo_pk):
     if request.method == 'POST':
         todo.delete()
         return redirect('currenttodos')
-
-
