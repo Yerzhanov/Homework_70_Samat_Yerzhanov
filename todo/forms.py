75c32417519bcfdf862e1fa764eb1c63cbf23c50
Yerzhanov <dearhauer@gmail.com>
2023-03-03 17:52:47 +0600

    Формы для комментариев, Тэгов, отношения многие ко многим

--- todo/forms.py
+++ todo/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
-from .models import Todolist
+from .models import Todolist, Comment
 
 
 class TodoForm(ModelForm):
     class Meta:
         model = Todolist
         fields = ['title', 'memo', 'important']
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['article', 'text', 'author', 'status', 'task_type', 'tags']
