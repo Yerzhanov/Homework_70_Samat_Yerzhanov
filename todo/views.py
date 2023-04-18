e5bd3889a6d6e1ae1f56a86b1a2ee86df9ef5c5d
Yerzhanov <dearhauer@gmail.com>
2023-04-18 16:43:08 +0600

    Создан API для просмотра, создания, обновления и удаления дел

--- todo/views.py
+++ todo/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth import login, logout, authenticate
 from django.urls import reverse
 from django.utils.http import urlencode
 from django.views.generic import CreateView, DetailView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .forms import TodoForm, CommentForm, SimpleSearchForm, ProjectForm, MyUserCreationForm
 from .models import Todolist, Comment, Projectlist
@@ -15,6 +17,9 @@ from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
+
+from rest_framework import viewsets, status
+from .serializer import TodoSerializer, ProjectSerializer
 
 
 class HomeView(TemplateView):
@@ -34,9 +39,11 @@ def signupuser(request):
                 login(request, user)
                 return redirect('currenttodos')
             except IntegrityError:
-                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Такой пользователь уже существует. Задайте новое имя.'})
+                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(),
+                                                                'error': 'Такой пользователь уже существует. Задайте новое имя.'})
         else:
             return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Пароль не совпадает'})
+
 
 class RegisterView(CreateView):
     model = User
@@ -63,16 +70,19 @@ def loginuser(request):
     else:
         user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
-            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Пользователь и пароль не найдены'})
+            return render(request, 'todo/loginuser.html',
+                          {'form': AuthenticationForm(), 'error': 'Пользователь и пароль не найдены'})
         else:
             login(request, user)
             return redirect('currenttodos')
+
 
 @login_required()
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
+
 
 @login_required()
 def createtodo(request):
@@ -86,7 +96,8 @@ def createtodo(request):
             new_to_do.save()
             return redirect('currenttodos')
         except ValueError:
-            return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error': 'Были переданы неверные данные. Попробуйте снова.'})
+            return render(request, 'todo/createtodo.html',
+                          {'form': TodoForm(), 'error': 'Были переданы неверные данные. Попробуйте снова.'})
 
 
 @login_required()
@@ -101,7 +112,8 @@ def viewtodo(request, todo_pk):
             form.save()
             return redirect('currenttodos')
         except ValueError:
-            return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': 'Неправильно введеные данные'})
+            return render(request, 'todo/viewtodo.html',
+                          {'todo': todo, 'form': form, 'error': 'Неправильно введеные данные'})
 
 
 @login_required()
@@ -116,14 +128,17 @@ def createcomment(request):
             new_comment.save()
             return redirect('currenttodos')
         except ValueError:
-            return render(request, 'todo/create_comment.html', {'form': CommentForm(), 'error': 'Были переданы неверные данные. Попробуйте снова.'})
+            return render(request, 'todo/create_comment.html',
+                          {'form': CommentForm(), 'error': 'Были переданы неверные данные. Попробуйте снова.'})
+
 
 @login_required()
 def viewcomment(request):
     comments = Comment.objects.all()
     return render(request, 'todo/view_comment.html', {'comments': comments})
 
-def deletecomment(request,article_pk):
+
+def deletecomment(request, article_pk):
     comments = get_object_or_404(Comment, pk=article_pk, user=request.user)
     if request.method == 'POST':
         comments.delete()
@@ -174,9 +189,9 @@ class CompleteTodosView(ListView):
     extra_context = {'title': 'Список выполненных дел'}
 
     def get_context_data(self, **kwargs):
-       context = super(CompleteTodosView, self).get_context_data()
-       context['todos'] = Todolist.objects.filter(date_completed__isnull=False).order_by('-date_completed')
-       return context
+        context = super(CompleteTodosView, self).get_context_data()
+        context['todos'] = Todolist.objects.filter(date_completed__isnull=False).order_by('-date_completed')
+        return context
 
 
 @login_required()
@@ -186,6 +201,7 @@ def completetodo(request, todo_pk):
         todo.date_completed = timezone.now()
         todo.save()
         return redirect('currenttodos')
+
 
 @login_required()
 def deletetodo(request, todo_pk):
@@ -232,9 +248,11 @@ class ProjectView(ListView):
             return self.form.cleaned_data['search']
         return None
 
+
 class ProjectDetailView(DetailView):
     template_name = 'todo/view_project.html'
     model = Projectlist
+
 
 class ProjectCreateView(LoginRequiredMixin, CreateView):
     template_name = 'todo/createproject.html'
@@ -243,6 +261,70 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
 
     def get_redirect_url(self):
         return reverse('list_project', kwargs={'pk': self.object.pk})
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todolist.objects.all()
+    serializer_class = TodoSerializer
+
+
+class TodoApiView(APIView):
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        todo = Todolist.objects.all()
+        return todo
+
+    def get_object(self, id):
+        return get_object_or_404(self.get_queryset(), id=id)
+
+    def get(self, request, pk=None,  *args, **kwargs):
+        id = pk or request.query_params.get('id')
+        if id:
+            serializer = TodoSerializer(self.get_object(id))
+        else:
+            serializer = TodoSerializer(self.get_queryset(), many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            todo = serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
+
+    def put(self, request, pk=None, *args, **kwargs):
+        todo_object = self.get_object(pk or request.query_params.get('id'))
+        data = request.data
+
+        todo_object.title = data["title"]
+        todo_object.memo = data["memo"]
+        todo_object.status = data["status"]
+
+        todo_object.save()
+
+        serializer = TodoSerializer(todo_object)
+        return Response(serializer.data)
+
+    def delete(self, request, pk=None):
+        todo_object = self.get_object(pk or request.query_params.get('id'))
+        todo_object.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+class DeleteTodoView(APIView):
+    def delete(self, request, id=None):
+        drinks = Todolist.objects.filter(id=id)
+        drinks.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Projectlist.objects.all()
+    serializer_class = ProjectSerializer
 
 # class ProjectUpdateView(UpdateView):
 #     model = Article
